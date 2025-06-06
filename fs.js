6febf6823a3bdf5cadea73f0ac639e2b46322cc4
Ormeau17 <61415085+Ormeau17@users.noreply.github.com>
2025-06-06 17:52:44 +1000

    fixed grip

--- fs.js
+++ fs.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <=127; i++) {
   let padded = i.toString().padStart(2, '0');
   console.log(
-    `<img src="images/grip_attack_lakside/grip_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
+    `<img src="images/grip_attack_lakeside/grip_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
   );
 }
