7172b19de6470d3373601f0f1c391e30a211b88d
Ormeau17 <61415085+Ormeau17@users.noreply.github.com>
2025-06-07 17:06:57 +1000

    new gallerys

--- ZZ_path_script.js
+++ ZZ_path_script.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <=140; i++) {
+for (let i = 1; i <=61; i++) {
   let padded = i.toString().padStart(2, '0');
   console.log(
-    `<img src="images/highball_cars_coffee_clayton/coffee_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
+    `<img src="images/dfo_meet/dfo_meet_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
   );
 }
