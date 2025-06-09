2cdf5ade60ba7863a2b00ecd2a223d1eeb7b41ef
Ormeau17 <61415085+Ormeau17@users.noreply.github.com>
2025-06-09 15:11:31 +1000

    added moto gp

--- ZZ_path_script.js
+++ ZZ_path_script.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <=61; i++) {
+for (let i = 1; i <=81; i++) {
   let padded = i.toString().padStart(2, '0');
   console.log(
-    `<img src="images/dfo_meet/dfo_meet_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
+    `<img src="images/moto_gp_2024/motogp_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
   );
 }
