93c683881c399acea74d699a4079f97ad258e713
Ormeau17 <61415085+Ormeau17@users.noreply.github.com>
2026-01-18 16:33:46 +1100

    added taggerty, fixed NZ

--- ZZ_path_script.js
+++ ZZ_path_script.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <=81; i++) {
+for (let i = 1; i <=343; i++) {
   let padded = i.toString().padStart(2, '0');
   console.log(
-    `<img src="images/moto_gp_2024/motogp_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
+    `<img src="images/highball_taggerty/taggerty_${padded}.jpg" alt="Image ${i}" class="gallery-img" onclick="openModal(this)">`
   );
 }
